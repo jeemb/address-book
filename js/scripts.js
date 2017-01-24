2fb170fa5809e5a274ba3962f44134f662b06c21
Dylan Stackhouse and Jennifer Beem <jennifer@>
2017-01-23 16:07:51 -0800

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 
 //business logic
-function Contact(first, last) {
+function Contact(first, last) { // new object constructor
   this.firstName = first;
   this.lastName = last;
   this.addresses = [];
 }
 
-function Address(street, city, state) {
+function Address(street, city, state) { //new object constructor
   this.street = street;
   this.city = city;
   this.state = state;
 }
-function resetFields() {
+function resetFields() { // new function to zero out input boxes after submission
   $("input#new-first-name").val("");
   $("input#new-last-name").val("");
   $("input.new-street").val("");
@@ -19,17 +19,17 @@ function resetFields() {
   $("input.new-state").val("");
 }
 
-Contact.prototype.fullName = function() {
+Contact.prototype.fullName = function() { // new method to compile first and last name into one
   return this.firstName + " " + this.lastName;
 }
 
-Address.prototype.fullAddress = function() {
+Address.prototype.fullAddress = function() { // new method to compile address specifics
   return this.street + ", " + this.city + ", " + this.state;
 }
 
 //user interface logic
 $(document).ready(function() {
-  $("#add-address").click(function() {
+  $("#add-address").click(function() { // when you click on the add address button, adds new input fields for additional addresses
       $("#new-addresses").append(
         '<div class="new-address">' +
          '<div class="form-group">' +
@@ -47,34 +47,37 @@ $(document).ready(function() {
        '</div>');
     });
 
-  $("form#new-contact").submit(function(event) {
+  $("form#new-contact").submit(function(event) { // prevents page from refreshing during submission event
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedFirstName = $("input#new-first-name").val(); // pulls inputted first name value into new variable
+    var inputtedLastName = $("input#new-last-name").val(); // pulls inputted last name value into new variable
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName); // creates new object from the acquired variables
 
     $(".new-address").each(function() {
-      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedStreet = $(this).find("input.new-street").val(); // declaring new variables to capture new inputs
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
-      newContact.addresses.push(newAddress);
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState); // new object to compile above declared variables into full address
+      newContact.addresses.push(newAddress); // associates address with contact
     });
 
-    $(".contacts").show();
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $(".contacts").show(); // display contacts section
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>"); // appends list items of newly inputted full names
 
-    $(".contact").last().click(function() {
+    $(".contact").last().click(function() { // makes newly inputted full names clickable to show full details and stores object info in event handler
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
-      $("ul#addresses").text("");
-        newContact.addresses.forEach(function(address) {
-          $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+
+      $("ul#addresses").text(""); // clears out address list so that the following can be added
+
+      newContact.addresses.forEach(function(address) { // loops through all addresses of contact and creates an appended list containing address details
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
       });
     });
+    resetFields(); // calls function above to reset all input boxes after submission
   });
 });
